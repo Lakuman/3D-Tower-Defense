4b0d615cc5df84b37e94d6dbaea5972b7d816265
AdrienRoussel <31723540+Lakuman@users.noreply.github.com>
2018-09-06 22:36:11 +0200

    Add a WaveSpawner
    - Wave Spawn
    - Prism / Nexus
    - Enemy Spawn every 5 secondes

---
+++ 3D Tower Defense/Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float speed = 10f;
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    private void Start()
+    {
+        target = WayPoints.points[0];
+
+    }
+    private void Update()
+    {
+        //First line gives us a direction between the enemy(target) and the next Waypoint
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized *speed* Time.deltaTime);
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+    void GetNextWaypoint()
+    {
+        if (wavepointIndex >= WayPoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        wavepointIndex++;
+        target = WayPoints.points[wavepointIndex];
+    }
+}
---
+++ 3D Tower Defense/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+
+     
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+    public float timeBetweenWaves = 5.5f;
+    private float countdown = 2f;
+    private int waveIndex = 0;
+
+    public Text waveCountdownText;
+
+    void Update()
+    {
+        if(countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+            return;
+        }
+        countdown -= Time.deltaTime;
+
+        countdown = Mathf.Clamp(countdown, 0f, Mathf.Infinity);
+
+        waveCountdownText.text = string.Format("{0:00}", countdown);
+    }
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+}
